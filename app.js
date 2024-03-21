5fe84d8a374ef59749da09c923f9d52e526c8132
James Lee <jimhlee23@gmail.com>
2024-03-21 14:35:14 -0400

    router setup

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
 const express = require("express");
 const { NotFoundError } = require("./expressError");
 
+const companiesRoutes = require('./routes/companies');
+const invoicesRoutes = require('./routes/invoices');
+
 const app = express();
 
+// Does this line parse json?
 app.use(express.json());
+app.use('/companies'.companiesRoutes);
+app.use('/invoices'.invoicesRoutes);
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
@@ -20,7 +26,6 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
-
 
 
 module.exports = app;
---
+++ routes/companies.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { NotFoundError } = require("./expressError");
+
+const router = new express.Router();
+
+
+
+module.exports = router
