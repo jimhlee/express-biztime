fd899adadc6d58aa9c795e243c146cf31e38aaa6
James Lee <jimhlee23@gmail.com>
2024-03-21 16:59:16 -0400

    complete post and put routes

--- routes/companies.js
+++ routes/companies.js
@@ -22,8 +22,8 @@ router.get('/:code', async function (req, res) {
 
   const result = await db.query(
     `SELECT code, name, description
-     FROM companies
-     WHERE code = $1`,
+    FROM companies
+    WHERE code = $1`,
     [req.params.code]
   );
   const company = result.rows[0];
@@ -33,8 +33,37 @@ router.get('/:code', async function (req, res) {
 });
 
 
+router.post('/', async function (req, res) {
+  if (!req.body) throw new BadRequestError();
 
+  const { code, name, description } = req.body;
+  const result = await db.query(
+    `INSERT INTO companies
+    (name, description)
+    VALUES ($1, $2, $3)
+    RETURNING code, name, description`,
+    [code, name, description],
+  );
+  const newCompany = result.rows[0];
+  return res.status(201).json({ newCompany });
+});
 
+router.put('/:code', async function (req, res) {
+  if (!req.params.code) throw new NotFoundError();
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+    SET name = $1,
+    description = $2
+    WHERE code = $3
+    RETURNING code, name, description`,
+    [name, description, req.params.code]
+    );
+    const updatedCompany = result.rows[0]
+    return res.json({ newCompany });
+
+});
 
 
 
