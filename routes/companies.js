fc71805049ecc0a03feccbf3a4c8d809159822ea
Emily Walker <ewalker415@gmail.com>
2024-03-21 14:58:03 -0700

    finished companies route

--- routes/companies.js
+++ routes/companies.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require("express");
 const { NotFoundError, BadRequestError } = require("../expressError");
 const db = require("../db");
@@ -18,7 +20,6 @@ router.get('/', async function (req, res) {
 
 /** GET / => {company: {code, name, description}, ...} */
 router.get('/:code', async function (req, res) {
-  if (!req.params.code) throw new NotFoundError();
 
   const result = await db.query(
     `SELECT code, name, description
@@ -27,19 +28,26 @@ router.get('/:code', async function (req, res) {
     [req.params.code]
   );
   const company = result.rows[0];
-  console.log(result.rows);
+
+  if (!company) throw new NotFoundError();
+
   return res.json({ company });
 
 });
 
-
+/** POST / => Receives {code, name, description}
+ * Returns {company: {code, name, description}}
+ */
 router.post('/', async function (req, res) {
   if (!req.body) throw new BadRequestError();
+
+  if (!req.name || !req.description) throw new BadRequestError();
+  if (Object.keys(req.body).length === 0) throw new BadRequestError();
 
   const { code, name, description } = req.body;
   const result = await db.query(
     `INSERT INTO companies
-    (name, description)
+    (code, name, description)
     VALUES ($1, $2, $3)
     RETURNING code, name, description`,
     [code, name, description],
@@ -48,8 +56,14 @@ router.post('/', async function (req, res) {
   return res.status(201).json({ newCompany });
 });
 
+/** PUT / => Receives {name, description}
+ * Returns {company: {code, name, description}} or 404
+ */
 router.put('/:code', async function (req, res) {
-  if (!req.params.code) throw new NotFoundError();
+
+  if (!Object.keys(req.body).length) throw new BadRequestError();
+  if (!req.name || !req.description) throw new BadRequestError();
+
   const { name, description } = req.body;
 
   const result = await db.query(
@@ -59,12 +73,33 @@ router.put('/:code', async function (req, res) {
     WHERE code = $3
     RETURNING code, name, description`,
     [name, description, req.params.code]
-    );
-    const updatedCompany = result.rows[0]
-    return res.json({ newCompany });
+  );
+
+  const updatedCompany = result.rows[0];
+
+  if (!updatedCompany) throw new NotFoundError();
+
+  return res.json({ updatedCompany });
 
 });
 
+/** DELETE / => Delete company
+ * Returns {status: "deleted"} or 404
+ */
+router.delete('/:code', async function (req, res) {
+  const result = await db.query(
+    `DELETE
+     FROM companies
+     WHERE code = $1
+     RETURNING name`,
+    [req.params.code]
+  );
+  const deletedRow = result.rows[0];
+
+  if (!deletedRow) throw new NotFoundError(`Not found: ${req.params.code}`);
+
+  return res.json({ status: "Deleted" });
+});
 
 
 
